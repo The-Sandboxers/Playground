cb1365ebb842ed9a65f90d6136d4d0cda14ad7af
Ava <ava@scherocman.com>
2025-02-03 00:25:47 -0500

    Update bonsai_load to reflect docs.json (example file) in esdata folder

--- bonsai_load.py
+++ bonsai_load.py
@@ -10,8 +10,8 @@ ELASTIC_PASSWORD = os.getenv('ELASTIC_PASSWORD')
 
 es = Elasticsearch('http://localhost:9200', basic_auth=("elastic", ELASTIC_PASSWORD))
 
-with open("esdata/") as file:
+with open("esdata/docs.json") as file:
     docs = json.loads(file.read())
     # print(docs)
-    helpers.bulk(es, docs )
+    helpers.bulk(es, docs)
     
