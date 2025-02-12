4d5445dc946c68dbcd2a4910d64cc9dc7d2df8b6
Ava <ava@scherocman.com>
2025-02-12 01:01:14 -0500

    Add formatting for game data in bonsai_load + update query in bonsai_q

--- bonsai_load.py
+++ bonsai_load.py
@@ -1,17 +1,59 @@
-from elasticsearch import Elasticsearch, helpers
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import streaming_bulk
 from dotenv import load_dotenv
 import os
 import json
 import re
+import ast
 
 load_dotenv()
-
 ELASTIC_PASSWORD = os.getenv('ELASTIC_PASSWORD')
+
+# create index in elasticsearch 
+# TODO AVA add the rest of the mappings here so we can search by them
+def create_index(es):
+    es.indices.create(
+        index="games", 
+        body={
+            "mappings":{
+                "properties":{
+                    "name":{"type":"text"},
+                }
+            }, 
+        },
+    )
+
+# map JSON array to readable JSON format for elasticsearch
+def format_data(games):
+    for g in games:
+        doc = {
+            'name': g['name']
+        }
+        yield doc
+
+
 
 es = Elasticsearch('http://localhost:9200', basic_auth=("elastic", ELASTIC_PASSWORD))
 
-with open("esdata/docs.json") as file:
-    docs = json.loads(file.read())
-    # print(docs)
-    helpers.bulk(es, docs)
+# grab json from file
+games=[]
+with open("esdata/game_data.json") as file:
+    #print(json.load(file))
+    #for line in file:
+    games += ast.literal_eval(json.load(file))
+
+num_games = len(games)
+
+print("Creating index")
+create_index(es)
+
+print("Indexing documents")
+
+successes=0
+for (ok, action) in streaming_bulk(
+    client=es, index="games", actions=format_data(games),
+):
+    successes += ok
+    print("Indexed %d/%d documents" % (successes, num_games))
+    
     
--- bonsai_query.py
+++ bonsai_query.py
@@ -10,7 +10,8 @@ ELASTIC_PASSWORD = os.getenv('ELASTIC_PASSWORD')
 
 es = Elasticsearch('http://localhost:9200', basic_auth=("elastic", ELASTIC_PASSWORD))
 
-res = es.search(index = "newsgroup", body={"query": {"more_like_this": {"fields": ["doc"], "like": "The first ice resurfacer was invented by Frank Zamboni, who was originally in the refrigeration business. Zamboni created a plant for making ice blocks that could be used in refrigeration applications. As the demand for ice blocks waned with the spread of compressor-based refrigeration, he looked for another way to capitalize on his expertise with ice production"}}})
+res = es.search(index = "games", query={"match":{"name":"star"}})
+
 print(len(res["hits"]["hits"]))
 for doc in res["hits"]["hits"]:
   print(doc)
