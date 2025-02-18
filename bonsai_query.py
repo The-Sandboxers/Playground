fe3460792b1641a56a6bd1c0981988d661e0076c
Ava <ava@scherocman.com>
2025-02-18 00:54:53 -0500

    Add addl example query for ElasticSearch

--- bonsai_query.py
+++ bonsai_query.py
@@ -10,9 +10,24 @@ ELASTIC_PASSWORD = os.getenv('ELASTIC_PASSWORD')
 
 es = Elasticsearch('http://localhost:9200', basic_auth=("elastic", ELASTIC_PASSWORD))
 
+# search for all games with "star" in the name
 res = es.search(index = "games", query={"match":{"name":"star"}})
 
 print(len(res["hits"]["hits"]))
 for doc in res["hits"]["hits"]:
   print(doc)
 
+# search for games that are not part of a bundle
+res = es.search(index = "games", query={
+  "bool":{
+    "must_not":{
+      "exists":{
+        "field": "bundles"
+      }
+    }
+  }
+})
+
+print(len(res["hits"]["hits"]))
+for doc in res["hits"]["hits"]:
+  print(doc)
