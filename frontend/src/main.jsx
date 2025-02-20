26b29d25663a1e4ae90aa6cdd37f63db7e0b6d1d
LukeGlaze <lukeg302@gmail.com>
2025-02-20 13:54:56 -0500

    Trying to signup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import Profile from './Profile.jsx'
+//import Profile from './Profile.jsx'
+import Signup from './Signup.jsx'
 // import App from './App.jsx'
-import Recomendations from './Recomendations.jsx'
+//import Recomendations from './Recomendations.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Recomendations />
+    <Signup />
   </StrictMode>,
 )
