0995bab699c46e90c3d2de01ccd39e70730dc9b6
Nick Carrino <94144414+Carrin0@users.noreply.github.com>
2025-03-04 03:18:40 -0500

    Frontend now allows users to cycle through random games. Also has button to profile page.

---
+++ frontend/src/ProfileButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function ProfileButton() {
+  const navigate = useNavigate();
+
+  return <button onClick={() => navigate('/profile')}>Profile</button>;
+}
+
+export default ProfileButton;
