82b7f0b76d0eee59807b4904ced82f9718e44a36
LukeGlaze <lukeg302@gmail.com>
2025-03-19 13:22:06 -0400

    fixed an issue with profile.jsx

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { requestBackend } from '../utils';
-import './Profile.css';
 
 export default function Profile()
 {
