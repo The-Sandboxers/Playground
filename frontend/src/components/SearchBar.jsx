8fe3448e8b4efc3a370bcb171a1a81dfa78caa03
Nick Carrino <94144414+Carrin0@users.noreply.github.com>
2025-03-20 13:51:25 -0400

    Recs page ported to tailwind

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+import axios from 'axios';
 
 function SearchBar() {
   const [query, setQuery] = useState('');
@@ -7,17 +7,14 @@ function SearchBar() {
 
   // Debounced API call effect
   useEffect(() => {
-
     const timer = setTimeout(() => {
       if (query) {
-        axios.get('http://127.0.0.1:5000/games/search',
-          { params: { search_term: query } }
-        )
-        .then((response) => {
-          console.log(response.data.result);
-          setResults(response.data.result);
-        })
-          .catch((error) => console.log('Error fetching data:', error));
+        axios.get('http://127.0.0.1:5000/games/search', { params: { search_term: query } })
+          .then(response => {
+            console.log(response.data.result);
+            setResults(response.data.result);
+          })
+          .catch(error => console.log('Error fetching data:', error));
       } else {
         setResults([]);
       }
@@ -27,7 +24,7 @@ function SearchBar() {
   }, [query]);
 
   return (
-    <div className="search-container">
+    <div className="relative">
       <input
         type="search"
         id="search-bar"
@@ -35,11 +32,12 @@ function SearchBar() {
         placeholder="The next game you'll love is..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="w-72 h-8 border border-red-500 bg-gray-200 text-black font-sans"
       />
       {results.length > 0 && (
-        <div className="search-results">
+        <div className="absolute top-full left-0 right-0 bg-white border border-gray-300 z-10">
           {results.map((item, index) => (
-            <div key={index} className="search-result-item">
+            <div key={index} className="p-0.5 cursor-pointer text-black opacity-80 hover:bg-gray-100">
               {item}
             </div>
           ))}
