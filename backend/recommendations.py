d47926b34b3bbd5ed90c864eba6cfe0f8236c015
Ava <ava@scherocman.com>
2025-03-26 00:13:46 -0400

    Create recommendation algorithm query

--- backend/recommendations.py
+++ backend/recommendations.py
@@ -6,18 +6,45 @@ load_dotenv()
 
 ELASTIC_PASSWORD = os.getenv('ELASTIC_PASSWORD')
 
-es = Elasticsearch('http://localhost:9200', basic_auth=("elastic", ELASTIC_PASSWORD))
+es = Elasticsearch('http://localhost:9200', basic_auth=("elastic", ELASTIC_PASSWORD), request_timeout=20)
 
-# hardcoded list of igdb ids to create recommendations off of for now
-played_games = [72, 71, 17000, 1879]
-for game in played_games:
-  index="games"
-  query={
-          "match":{
-            "igdb_id": game
+# parameter played_games: list of igdb_ids
+def recommendation_algorithm(played_games):
+  query_docs = []
+  for game in played_games:
+    index="games"
+    query={
+            "match":{
+              "igdb_id": game
+            }
           }
-        }
-  fields=["name"]
-  result = es.search(index=index, query=query, fields=fields)
-  for doc in result["hits"]["hits"]:
-    print(doc["_source"]["name"])
+    fields=["name"]
+    result = es.search(index=index, query=query, fields=fields)
+
+    # create query object of elasticsearch IDs for all games to use in querying
+    for doc in result["hits"]["hits"]:
+      doc_id = doc["_id"]
+      dict_item = {}
+      dict_item["_id"]=doc_id
+      query_docs.append(dict_item)
+
+
+  query ={
+    "more_like_this" : {
+      "fields" : ["keywords"],
+      "like" : query_docs,
+      "min_term_freq" : 0,
+      "min_doc_freq" : 1,
+      "minimum_should_match": '20%',
+    }
+  }
+
+  result = es.search(index=index, query=query)
+  return result
+  
+def main():
+    recommendation_algorithm()
+
+if __name__ == "__main__":
+    main()
+  
