69f60cc5f5891a88ebee36348917c2efd2fec880
LukeGlaze <lukeg302@gmail.com>
2025-03-26 18:57:41 -0400

    Added pictures of liked and played games to profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -11,16 +11,21 @@ export default function Profile()
     const [profilePic, setProfilePic] = useState("https://tr.rbxcdn.com/38c6edcb50633730ff4cf39ac8859840/420/420/Hat/Png");
     const [likedGames, setLikedGames] = useState("");
     const [playedGames, setPlayedGames] = useState("");
+    const [playedGamesData, setPlayedGamesData] = useState([]);
+    const [likedGamesData, setLikedGamesData] = useState([]);
 
 
     useEffect(() => {
         const fetchProfile = async () => {
             try {
                 const {success, data} = await requestBackend("GET", "http://127.0.0.1:5000/profile");
+                console.log(data)
                 if (success){
                     setUsername(data.username);
                     setLikedGames(data.liked_games);
                     setPlayedGames(data.all_games);
+                    setPlayedGamesData(data.all_games_sources);
+                    setLikedGamesData(data.liked_games_sources);
                 }
                 
             } catch (error) {
@@ -61,22 +66,34 @@ export default function Profile()
 
 
     return (
-        <div className="mt-18 grid grid-flow-col grid-rows-3 grid-cols-5 gap-4 p-5 font-black text-gray-200 text-lg">
+        <div className="mt-18 grid grid-flow-col grid-rows-3 grid-cols-5 gap-4 p-5 font-black text-gray-200 text-2xl">
             <div className="col-span-2 row-span-3 rounded-lg bg-card-foreground p-5">
                 <h3>{username}</h3>
                 <img src={profilePic} className="rounded-full mx-auto"></img>
                 
-                <Button size="lg" variant="secondary" className="font-black text-md" onClick={signOut}>Sign Out</Button>
-                <h3>Linked Services:</h3>
-                <Button size="lg" variant="secondary" className="font-black text-md" onClick={steamAuthenticate}>Connect to Steam</Button>
+                <Button size="lg" variant="secondary" className="font-black text-md mb-4" onClick={signOut}>Sign Out</Button>
+                <h3 className="mb-4">Linked Services:</h3>
+                <Button size="lg" variant="secondary" className="font-black text-md mb-4" onClick={steamAuthenticate}>Connect to Steam</Button>
             </div>
             <div className="col-span-3 rounded-lg bg-foreground p-5">
-                <h3>Played Games</h3>
-                <p>{playedGames}</p>
+                <p className="mb-4">Played Games</p>
+                <div className="flex space-x-4">
+                    {playedGamesData.map((element, index) => (
+                    <div key={index} className="">
+                        <img src={element.cover_url}/>
+                    </div>
+                    ))}
+                </div>
             </div>
             <div className="col-span-3 rounded-lg bg-foreground p-5">
-                <h3>Liked Games</h3>
-                <p>{likedGames}</p>
+                <p className="mb-4">Liked Games</p>
+                <div className="flex space-x-4">
+                    {likedGamesData.map((element, index) => (
+                    <div key={index} className="">
+                        <img src={element.cover_url}/>
+                    </div>
+                    ))}
+                </div>
             </div>
         </div>
       );
