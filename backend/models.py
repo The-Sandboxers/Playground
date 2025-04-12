4e2e8028572660388cc393943ddceb757be9a167
Victor Fawole <victorfawole@gmail.com>
2025-04-12 16:23:33 -0400

    Added migrations and new platforms to model

--- backend/models.py
+++ backend/models.py
@@ -17,9 +17,16 @@ class User(db.Model):
     password_hash: Mapped[str] = mapped_column(String(255))
     steam_id: Mapped[Optional[str]] = mapped_column(String(17))
     games: Mapped[List["UserGame"]] = relationship(back_populates="user")
-    show_pc: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    show_windows: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
     show_xbox: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    show_playstation_5: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    show_xbox_series_sx: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    show_playstation_4: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    show_xbox_one: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    show_linux: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    show_mac: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     show_switch: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    
     def to_dict(self):
         return {"id": self.id, "username": self.username, "email": self.email, "steam_id": self.steam_id}
     
